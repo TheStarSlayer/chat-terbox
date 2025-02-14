064e68d6b4b92c390d41ef0f710d1135feab16b4
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-14 18:28:04 +0530

    Implement Sign-Up API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,51 @@
-export const signup = (req, res) => {
-    res.send("sign-up route");
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+    try {
+        const {fullName, email, password} = req.body;
+
+        if (!fullName || !email || !password)
+            return res.status(400).json({ message: "All fields are required!" });
+
+        if (password.length < 6)
+            return res.status(400).json({ message: "All fields are required!" });
+
+        const user = await User.findOne(email);
+
+        if (user)
+            return res.status(400).json({ message: "User already exists!" });
+
+        const salt = await bcrypt.salt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = User.create({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(200).json({
+                _id: (await newUser)._id,
+                fullName: (await newUser).fullName,
+                email: (await newUser).email,
+                profilePic: newUser.profilePic
+            });
+        }
+        else {
+            return res.status(400).json({ message: "User data invalid!" });
+        }
+        
+    }
+    catch (error) {
+        console.log(`Error in sign-up controller: ${error.message}`);
+        return res.status(400).json({ message: "Internal Server Error" });
+    }
 };
 
 export const login = (req, res) => {
