8c5c61e2451e91e5d0b3cad6dd6947a4ab3faaad
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-14 22:35:18 +0530

    Use cookieParser to parse cookies

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config(); // Access .env variables
 const app = express(); // Create express instance
 
 const PORT = process.env.PORT; // Import port from .env
 
+app.use(cookieParser());
 app.use(express.json()); // Returns body as JSON
 app.use("/api/auth", authRoutes); // Use authentication router 
 
