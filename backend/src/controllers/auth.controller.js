c5f3266acd9194f07a1f037fe5c7072ef05c9c37
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-14 18:42:37 +0530

    Fix Bugs: Set proper functions and pass correct parameters

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/token.js";
 
 export const signup = async (req, res) => {
     try {
@@ -12,15 +13,15 @@ export const signup = async (req, res) => {
         if (password.length < 6)
             return res.status(400).json({ message: "All fields are required!" });
 
-        const user = await User.findOne(email);
+        const user = await User.findOne({email});
 
         if (user)
             return res.status(400).json({ message: "User already exists!" });
 
-        const salt = await bcrypt.salt(10);
+        const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = User.create({
+        const newUser = new User({
             fullName,
             email,
             password: hashedPassword
@@ -31,9 +32,9 @@ export const signup = async (req, res) => {
             await newUser.save();
 
             res.status(200).json({
-                _id: (await newUser)._id,
-                fullName: (await newUser).fullName,
-                email: (await newUser).email,
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
                 profilePic: newUser.profilePic
             });
         }
