884281190b40fb93ada2cb3f6d79fbdcb76480a3
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-14 17:32:41 +0530

    Add user schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        fullName: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6
+        },
+        profilePic: {
+            type: String,
+            required: true,
+            default: ""
+        }
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
