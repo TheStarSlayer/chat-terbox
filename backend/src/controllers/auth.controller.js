2f7a11aad81d5dbc84b3ce5ce3c329d8e3be868d
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-14 17:44:23 +0530

    Add dummy controllers

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("sign-up route");
+};
+
+export const login = (req, res) => {
+    res.send("log-in route");
+};
+
+export const logout = (req, res) => {
+    res.send("log-out route");
+};
