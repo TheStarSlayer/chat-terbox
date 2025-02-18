caee2b3d8ace7651870139364f7c776c7da7cfe8
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-18 16:14:34 +0530

    Implement getUsersForSidebar API

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config(); // Access .env variables
 const app = express(); // Create express instance
@@ -11,7 +12,8 @@ const PORT = process.env.PORT; // Import port from .env
 
 app.use(cookieParser());
 app.use(express.json()); // Returns body as JSON
-app.use("/api/auth", authRoutes); // Use authentication router 
+app.use("/api/auth", authRoutes); // Use authentication router
+app.use("/api/message", messageRoutes);
 
 // Open app in PORT
 app.listen(PORT, () => {
