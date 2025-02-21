28c6966125067a88cb4a1e7178d84854498adcca
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-02-21 20:07:52 +0530

    Send createdAt when log-in

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,7 +70,8 @@ export const login = async (req, res) => {
             _id: user._id,
             fullName: user.fullName,
             email: user.email,
-            profilePic: user.profilePic
+            profilePic: user.profilePic,
+            createdAt: user.createdAt
         });
     }
     catch (error) {
