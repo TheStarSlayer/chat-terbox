0605a0ca3e974b3fcfcb7b669acc3ec1b49738d0
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-03-03 18:21:13 +0530

    Increase data limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,8 @@ dotenv.config(); // Access .env variables
 const PORT = process.env.PORT; // Import port from .env
 
 app.use(cookieParser());
-app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 app.use(cors({
     origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true
