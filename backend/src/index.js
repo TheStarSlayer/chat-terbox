7f9497102a97e6228ae3b0bebd6fd4f030483c64
Lalith Sri Sai Manda <113292968+TheStarSlayer@users.noreply.github.com>
2025-03-03 19:36:12 +0530

    Project complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,12 @@ import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config(); // Access .env variables
 
 const PORT = process.env.PORT; // Import port from .env
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json({ limit: "2mb" }));
@@ -21,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes); // Use authentication router
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 // Open app in PORT
 server.listen(PORT, () => {
